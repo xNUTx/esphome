a957d1bd1bde24d6463f87085121a45d0b924db0
[NUT] <nut@fun-industries.nl>
2025-01-13 11:12:25 +0100

    Trigger...

--- esphome/components/dfplayer/dfplayer.cpp
+++ esphome/components/dfplayer/dfplayer.cpp
@@ -245,8 +245,7 @@ void DFPlayer::loop() {
             break;
           case 0x4C:
             ESP_LOGV(TAG, "Reply received on Query, cmd %#02x arg %04d", cmd, argument);
-            this->on_query_track_callback_.call(argument);
-            this->returned_track_ = argument;
+            this->fan_trigger_->trigger(argument);
             break;
           case 0x3D:
             ESP_LOGV(TAG, "Playback finished");
