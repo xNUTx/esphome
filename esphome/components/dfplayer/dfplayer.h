8acea34e9865b45493778e218d5de01bcd56a8e5
[NUT] <nut@fun-industries.nl>
2025-01-17 15:30:18 +0100

    Update dfplayer.h

--- esphome/components/dfplayer/dfplayer.h
+++ esphome/components/dfplayer/dfplayer.h
@@ -180,7 +180,7 @@ DFPLAYER_SIMPLE_ACTION(VolumeUpAction, volume_up)
 DFPLAYER_SIMPLE_ACTION(VolumeDownAction, volume_down)
 DFPLAYER_SIMPLE_ACTION(QueryTrackAction, query_track)
 DFPLAYER_SIMPLE_ACTION(QueryVolumeAction, get_volume)
-DFPLAYER_SIMPLE_ACTION(QueryEqAction, get_equalizer)
+DFPLAYER_SIMPLE_ACTION(QueryEqualizerAction, get_equalizer)
 
 template<typename... Ts> class DFPlayerIsPlayingCondition : public Condition<Ts...>, public Parented<DFPlayer> {
  public:
